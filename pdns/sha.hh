0ea73580ff231e4e951f6bbf02dd982f4b40d1f5
Kees Monshouwer <mind04@monshouwer.org>
2020-12-30 21:22:44 +0100

    SHA helper

--- pdns/sha.hh
+++ pdns/sha.hh
@@ -20,9 +20,12 @@
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
  */
 #pragma once
+#ifdef HAVE_CONFIG_H
+#include "config.h"
+#endif
 #include <string>
-#include <stdint.h>
 #include <openssl/sha.h>
+#include <openssl/evp.h>
 
 inline std::string pdns_sha1sum(const std::string& input)
 {
@@ -51,3 +54,78 @@ inline std::string pdns_sha512sum(const std::string& input)
   SHA512(reinterpret_cast<const unsigned char*>(input.c_str()), input.length(), result);
   return std::string(result, result + sizeof result);
 }
+
+
+class pdns_SHA
+{
+protected:
+  pdns_SHA(const EVP_MD *m) : md(m) {
+#if defined(HAVE_EVP_MD_CTX_NEW) && defined(HAVE_EVP_MD_CTX_FREE)
+    mdctx = std::unique_ptr<EVP_MD_CTX, void(*)(EVP_MD_CTX*)>(EVP_MD_CTX_new(), EVP_MD_CTX_free);
+#else
+    mdctx = std::unique_ptr<EVP_MD_CTX, void(*)(EVP_MD_CTX*)>(EVP_MD_CTX_create(), EVP_MD_CTX_destroy);
+#endif
+    if (!mdctx) {
+      throw std::runtime_error(std::string(EVP_MD_name(md)) + " EVP context initialization failed");
+    }
+    this->clear();
+  }
+public:
+  void clear() {
+    if (EVP_DigestInit_ex(mdctx.get(), md, nullptr) != 1) {
+      throw std::runtime_error(std::string(EVP_MD_name(md)) + " EVP digest init failed");
+    }
+  }
+  void update(const std::string& input) {
+    if (EVP_DigestUpdate(mdctx.get(), input.data(), input.size()) != 1) {
+      throw std::runtime_error(std::string(EVP_MD_name(md)) + " EVP digest update failed");
+    }
+  }
+  std::string hash() {
+    unsigned char md_value[EVP_MAX_MD_SIZE];
+    unsigned int md_len;
+#if defined(HAVE_EVP_MD_CTX_NEW) && defined(HAVE_EVP_MD_CTX_FREE)
+    auto mdout = std::unique_ptr<EVP_MD_CTX, void(*)(EVP_MD_CTX*)>(EVP_MD_CTX_new(), EVP_MD_CTX_free);
+#else
+    auto mdout = std::unique_ptr<EVP_MD_CTX, void(*)(EVP_MD_CTX*)>(EVP_MD_CTX_create(), EVP_MD_CTX_destroy);
+#endif
+    if (!mdout) {
+      throw std::runtime_error(std::string(EVP_MD_name(md)) + " EVP context new failed");
+    }
+    if (EVP_MD_CTX_copy_ex(mdout.get(), mdctx.get()) != 1) {
+      throw std::runtime_error(std::string(EVP_MD_name(md)) + " EVP context copy failed");
+    }
+    if (EVP_DigestFinal_ex(mdout.get(), md_value, &md_len) != 1) {
+      throw std::runtime_error(std::string(EVP_MD_name(md)) + " EVP digest final failed");
+    }
+    return std::string(md_value, md_value + md_len);
+  }
+private:
+  const EVP_MD *md;
+  std::unique_ptr<EVP_MD_CTX, void (*)(EVP_MD_CTX*)> mdctx{nullptr,nullptr};
+};
+
+class pdns_SHA1 : public pdns_SHA {
+public:
+  pdns_SHA1() : pdns_SHA(EVP_sha1()) {}
+};
+
+class pdns_SHA224 : public pdns_SHA {
+public:
+  pdns_SHA224() : pdns_SHA(EVP_sha224()) {}
+};
+
+class pdns_SHA256 : public pdns_SHA {
+public:
+  pdns_SHA256() : pdns_SHA(EVP_sha256()) {}
+};
+
+class pdns_SHA384 : public pdns_SHA {
+public:
+  pdns_SHA384() : pdns_SHA(EVP_sha384()) {}
+};
+
+class pdns_SHA512 : public pdns_SHA {
+public:
+  pdns_SHA512() : pdns_SHA(EVP_sha512()) {}
+};
